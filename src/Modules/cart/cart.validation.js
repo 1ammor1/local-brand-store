aa51aed00595438fd5d337a0d09508dd6208ba98
Amar Haikal <mr.ammar2019@gmail.com>
2025-07-17 16:51:40 +0300

    Update cart.validation.js -- adding color size

--- src/Modules/cart/cart.validation.js
+++ src/Modules/cart/cart.validation.js
@@ -1,9 +1,12 @@
 import Joi from "joi";
 
 export const addToCartSchema = Joi.object({
-  productId: Joi.string().length(24).hex().required(), 
-  quantity: Joi.number().integer().min(1).default(1)
+  productId: Joi.string().length(24).hex().required(),
+  quantity: Joi.number().integer().min(1).default(1),
+  color: Joi.string().required(),
+  size: Joi.string().required()
 });
+
 
 export const removeFromCartSchema = Joi.object({
   productId: Joi.string().length(24).hex().required(), 
