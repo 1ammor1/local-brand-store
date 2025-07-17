e8a42bb0aa9267e37786cb752425dc47d0670a60
Amar Haikal <mr.ammar2019@gmail.com>
2025-07-17 16:46:45 +0300

    Update cart.model.js -- #1 ItemSchema

--- src/DB/models/cart.model.js
+++ src/DB/models/cart.model.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 const cartItemSchema = new mongoose.Schema({
   product: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
   quantity: { type: Number, default: 1 },
+  color: { type: String, required: true },
+  size: { type: String, required: true },
 });
+
 
 const cartSchema = new mongoose.Schema(
   {
