22553c3ac12716644ac60edf90b338e584960900
Amar Haikal <mr.ammar2019@gmail.com>
2025-07-18 16:46:58 +0300

    Update cart.validation.js

--- src/Modules/cart/cart.validation.js
+++ src/Modules/cart/cart.validation.js
@@ -10,6 +10,8 @@ export const addToCartSchema = Joi.object({
 
 export const removeFromCartSchema = Joi.object({
   productId: Joi.string().length(24).hex().required(), 
+  color: Joi.string().required(),
+  size: Joi.string().required()
 });
 
 export const updateQuantityBodySchema = Joi.object({
