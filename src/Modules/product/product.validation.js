7de48a93bbe97f7b27770c20040d3851a64816f4
Amar Haikal <mr.ammar2019@gmail.com>
2025-07-19 22:45:53 +0300

    Update product.validation.js

--- src/Modules/product/product.validation.js
+++ src/Modules/product/product.validation.js
@@ -4,22 +4,20 @@ export const createProductSchema = Joi.object({
   title: Joi.string().min(3).max(100).required(),
   description: Joi.string().max(1000).required(),
   originalPrice: Joi.number().positive().required(),
+  price: Joi.number().positive().optional(),
 
   discount: Joi.object({
     type: Joi.string().valid("percentage", "fixed").required(),
     amount: Joi.number().positive().required()
   }).optional(),
 
-  category: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
+  category: Joi.string().pattern(/^[0-9a-fA-F]{24}$/).required(),
 
-  variants: Joi.array().items(
-    Joi.object({
-      color: Joi.string().required(),
-      size: Joi.string().required(),
-      quantity: Joi.number().integer().min(0).required()
-    })
-  ).min(1).required()
+  sizes: Joi.array().items(Joi.string()).min(1).required(),
+  colors: Joi.array().items(Joi.string()).min(1).required(),
+  quantity: Joi.number().integer().min(1).required(),
 });
+
 
 
 // 🆙 لتحديث منتج
