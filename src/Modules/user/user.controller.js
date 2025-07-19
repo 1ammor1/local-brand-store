9ec9197add27b9cebaf8755bffe44cf842153585
Amar Haikal <mr.ammar2019@gmail.com>
2025-07-19 20:51:16 +0300

    Update user.controller.js

--- src/Modules/user/user.controller.js
+++ src/Modules/user/user.controller.js
@@ -5,14 +5,24 @@ export const getProfile = async (req, res) => {
   const user = await UserModel.findById(req.user.id).select("-password");
   res.status(200).json({ user });
 };
+
 export const updateProfile = async (req, res) => {
-  const { firstName, lastName , oldPassword, newPassword } = req.body;
+  const { firstName, lastName, oldPassword, newPassword, email } = req.body;
   const user = await UserModel.findById(req.user.id);
 
   if (!user) return res.status(404).json({ message: "User not found" });
 
   if (firstName) user.firstName = firstName;
   if (lastName) user.lastName = lastName;
+
+  if (email && email !== user.email) {
+    const existingEmail = await UserModel.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ message: "Email is already in use" });
+    }
+    user.email = email;
+    user.confirmEmail = false;
+  }
 
   if (oldPassword && newPassword) {
     const match = await bcrypt.compare(oldPassword, user.password);
@@ -24,7 +34,8 @@ export const updateProfile = async (req, res) => {
 
   await user.save();
   res.status(200).json({ message: "Profile updated successfully" });
-}; 
+};
+
 
 
 export const logout = async (req, res) => {
