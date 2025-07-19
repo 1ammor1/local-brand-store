7549a6d30017c4ba83f5ac8b87f398f470d08561
Amar Haikal <mr.ammar2019@gmail.com>
2025-07-19 22:22:13 +0300

    Update product.model.js

--- src/DB/models/product.model.js
+++ src/DB/models/product.model.js
@@ -1,39 +1,34 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema(
-  {
-    title: String,
-    description: String,
-    originalPrice: Number,
-    price: Number,
-    discount: {
-      amount: Number,
-      type: { type: String, enum: ["percentage", "fixed"] }
-    },
-    quantity: Number,
-    category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
-    images: [
-      {
-        url: String,
-        public_id: String,
-      }
-    ],
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    colors: {
-      type: [String],
-      default: [],
-    },
-    size: {
-      type: [String],
-      default: [],
-    },
+const productSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  originalPrice: Number,
+  price: Number,
+  discount: {
+    amount: Number,
+    type: { type: String, enum: ["percentage", "fixed"] }
   },
-  {
-    timestamps: true,
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true },
-  }
-);
+  // الكمية لكل variant (color + size)
+  variants: [{
+    color: String,
+    size: String,
+    quantity: { type: Number, default: 0 }
+  }],
+  category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+  images: [
+    {
+      url: String,
+      public_id: String,
+    }
+  ],
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+}, {
+  timestamps: true,
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+});
+
 
 productSchema.virtual("imageUrl").get(function () {
   return this.images?.[0]?.url || null;
