45c519fe708c86e13e2e756e17cf412c1adec554
Amar Haikal <mr.ammar2019@gmail.com>
2025-07-19 20:49:41 +0300

    Update user.validation.js

--- src/Modules/user/user.validation.js
+++ src/Modules/user/user.validation.js
@@ -3,6 +3,8 @@ import Joi from "joi";
 export const updateProfileSchema = Joi.object({
   firstName: Joi.string().min(3).max(50).optional(),
   lastName: Joi.string().min(3).max(50).optional(),
+  email: Joi.string().email().optional(),
+
   oldPassword: Joi.string().min(6).optional(),
   newPassword: Joi.when("oldPassword", {
     is: Joi.exist(),
