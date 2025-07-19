d6783b0f298d743921bd6ee0b5e09c1cd33c3e6b
Amar Haikal <mr.ammar2019@gmail.com>
2025-07-19 19:20:58 +0300

    Update order.validation.js

--- src/Modules/order/order.validation.js
+++ src/Modules/order/order.validation.js
@@ -2,20 +2,18 @@ import Joi from "joi";
 import { Governorates } from "../../utils/governorates.js";
 
 export const createOrderSchema = Joi.object({
-  shippingAddress: Joi.object({
-    fullName: Joi.string().min(3).max(100).required(),
-    phone: Joi.string().pattern(/^\+?[0-9]{7,15}$/).required(),
-    anotherPhone: Joi.string().pattern(/^\+?[0-9]{7,15}$/).optional(),
-    addressLine: Joi.string().min(5).max(255).required(),
-    city: Joi.string().min(2).max(100).required(),
-    gov: Joi.string().valid(...Governorates).required(),
-    country: Joi.string().default("Egypt").optional(),
-  }).required(),
+  fullName: Joi.string().min(3).max(100).required(),
+  phone: Joi.string().pattern(/^\+?[0-9]{7,15}$/).required(),
+  anotherPhone: Joi.string().pattern(/^\+?[0-9]{7,15}$/).optional(),
+  addressLine: Joi.string().min(5).max(255).required(),
+  city: Joi.string().min(2).max(100).required(),
+  gov: Joi.string().valid(...Governorates).required(),
+  country: Joi.string().default("Egypt").optional(),
 
   notes: Joi.string().allow("").optional(),
-
-  paymentMethod: Joi.string().valid("cash", "card").default("cash")
+  paymentMethod: Joi.string().valid("cash", "card").default("cash"),
 });
+
 
 export const updateOrderStatusSchema = Joi.object({
   id: Joi.string().length(24).hex().required(),
