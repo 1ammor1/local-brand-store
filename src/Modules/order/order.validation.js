0bbe7383082fe06d1e8deeda0f02e549e399f529
Amar Haikal <mr.ammar2019@gmail.com>
2025-07-20 03:56:37 +0300

    Update order.validation.js

--- src/Modules/order/order.validation.js
+++ src/Modules/order/order.validation.js
@@ -4,7 +4,7 @@ import { Governorates } from "../../utils/governorates.js";
 export const createOrderSchema = Joi.object({
   fullName: Joi.string().min(3).max(100).required(),
   phone: Joi.string().pattern(/^\+?[0-9]{7,15}$/).required(),
-  anotherPhone: Joi.string().allow("").pattern(/^\+?[0-9]{7,15}$/).optional(),
+  anotherPhone: Joi.string().allow("", null).pattern(/^\+?[0-9]{7,15}$/).optional(),
   addressLine: Joi.string().min(5).max(255).required(),
   city: Joi.string().min(2).max(100).required(),
   gov: Joi.string().valid(...Governorates).required(),
